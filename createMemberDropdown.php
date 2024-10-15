b822e09c8d6d20ddb4ffab346744c320a1a08564
Nora Klarin <B00177535@mytudublin.ie>
2024-10-15 15:37:26 +0100

    part 2 dropdown added

---
+++ createMemberDropdown.php
@@ -0,0 +1,18 @@
+<?php 
+include("dbcon.php");
+
+$sql = "SELECT * from member";
+$result = mysqli_query($conn, $sql);
+
+echo "<SELECT name ='memberID'>";
+
+while($row = mysqli_fetch_assoc($result)) {
+    $id=$row['id'];
+    $fn=$row['firstname'];
+    $sn=$row['surname'];
+    echo "<option value= '$id'> $fn $sn</option>";
+}
+echo "</SELECT>";
+
+mysqli_close($conn); 
+?>
